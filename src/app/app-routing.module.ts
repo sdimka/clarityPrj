e9536a5ca5e8cd47ed5bfe1dace353193d234336
sd <s_d_a@mail.ru>
2019-03-30 01:14:30 +0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainForm } from './mainForm/mainForm.component'
+
+const routes: Routes = [
+  { path: '', component: MainForm},
+  //{ path: 'complete', component: CompleteComponent},
+  //{ path: 'completeNew', component: CompleteNewComponent},
+  //{ path: '**', component: CompleteComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/mainForm/mainForm.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+
+@Component({
+    selector: 'app-MainForm',
+    templateUrl: './mainForm.component.html',
+    styleUrls: ['./mainForm.component.css']
+  })
+  
+  export class MainForm implements OnInit {
+    ngOnInit() {
+
+    }
+  }
